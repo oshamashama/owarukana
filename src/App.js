dc8ada31b65d6c099cc105d87a107ef1e6e060ab
oshamashama <65126083+oshamashama@users.noreply.github.com>
2024-02-19 03:02:52 +0900

    fix : hide past limit(after 3 days)
    close #1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import './App.css';
-import { useState, useEffect } from 'react'
-import Data from './deadline.json';
-import {
-  TwitterShareButton,
-  XIcon,
-} from "react-share"
-import { FaGithub } from "react-icons/fa"
+import "./App.css";
+import { useState, useEffect } from "react";
+import datajson from "./deadline.json";
+import { TwitterShareButton, XIcon } from "react-share";
+import { FaGithub } from "react-icons/fa";
 
+const Data = datajson.data.filter((d) => {
+  const dd = new Date(d.year, d.month - 1, d.day + 3, d.hour, d.minute);
+  const now = new Date();
+  return dd > now;
+});
 const App = () => {
   const [mode, setMode] = useState(0);
   const [type, setType] = useState(0);
-
-  const limit = new Date(Data.data[type].year, Data.data[type].month - 1, Data.data[type].day, Data.data[type].hour, Data.data[type].minute,)
+  const limit = new Date(
+    Data[type].year,
+    Data[type].month - 1,
+    Data[type].day,
+    Data[type].hour,
+    Data[type].minute
+  );
   const [data, setData] = useState((limit.getTime() - Date.now()) / 1000);
 
   const mode_array = [
@@ -19,55 +26,67 @@ const App = () => {
     [1000 * 60, "分"],
     [1000 * 60 * 60, "時間"],
     [1000 * 60 * 60 * 24, "日"],
-    [1000 * 60 * 60 * 24 * 365, "年"]
+    [1000 * 60 * 60 * 24 * 365, "年"],
   ];
 
-  useEffect(() => {
+  useEffect(
+    () => {
+      const interval = setInterval(() => {
+        const now = new Date();
+        const lim = new Date(
+          Data[type].year,
+          Data[type].month - 1,
+          Data[type].day,
+          Data[type].hour,
+          Data[type].minute
+        );
+        setData(lim.getTime() - now.getTime());
+      }, 50);
 
-    const interval = setInterval(() => {
-      const now = new Date();
-      const lim = new Date(Data.data[type].year,
-        Data.data[type].month - 1,
-        Data.data[type].day,
-        Data.data[type].hour,
-        Data.data[type].minute,);
-      setData((lim.getTime() - now.getTime()));
-
-    }, 50);
-
-    return () => clearInterval(interval);
-  },
+      return () => clearInterval(interval);
+    },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [mode, type]
   );
 
-  const url = 'https://oshamashama.github.io/owarukana/'
-  const content = `${Data.data[type].name} まで，あと${(data / mode_array[mode][0]).toFixed(3)} ${mode_array[mode][1]}`
+  const url = "https://oshamashama.github.io/owarukana/";
+  const content = `${Data[type].name} まで，あと${(data / mode_array[mode][0]).toFixed(3)} ${mode_array[mode][1]}`;
 
-
-  return <>
-    <div className="App">
-      <div
-        role="button"
-        tabIndex="0"
-        onClick={() => setType((type + 1) % Data.data.length)}> {Data.data[type].name} まで，あと </div>
-      <div
-        role="button"
-        tabIndex="0"
-        onClick={() => setMode((mode + 1) % mode_array.length)}> {(data / mode_array[mode][0]).toFixed(3)} {mode_array[mode][1]}
-      </div>
-
-      <div className='info'>
-        <TwitterShareButton url={url} title={`${content}`} hashtags={['owarukana']}
+  return (
+    <>
+      <div className="App">
+        <div
+          role="button"
+          tabIndex="0"
+          onClick={() => setType((type + 1) % Data.length)}
         >
-          <XIcon size={60} round />
-        </TwitterShareButton>
-        <a href={'https://github.com/oshamashama/owarukana/'}>
-          <FaGithub size={60} color='#FFFFFF' />
-        </a>
+          {" "}
+          {Data[type].name} まで，あと{" "}
+        </div>
+        <div
+          role="button"
+          tabIndex="0"
+          onClick={() => setMode((mode + 1) % mode_array.length)}
+        >
+          {" "}
+          {(data / mode_array[mode][0]).toFixed(3)} {mode_array[mode][1]}
+        </div>
+
+        <div className="info">
+          <TwitterShareButton
+            url={url}
+            title={`${content}`}
+            hashtags={["owarukana"]}
+          >
+            <XIcon size={60} round />
+          </TwitterShareButton>
+          <a href={"https://github.com/oshamashama/owarukana/"}>
+            <FaGithub size={60} color="#FFFFFF" />
+          </a>
+        </div>
       </div>
-    </div >
-  </>;
-}
+    </>
+  );
+};
 
 export default App;
